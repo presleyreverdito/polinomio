784891bedfaa1c407efbab0ace849740e521aa22
Presley Demuner Reverdito <presleyreverdito@gmail.com>
2017-01-22 20:02:48 -0300

    Foram resolvidos alguns problemas com o arquivo main_lista

--- main_lista.c
+++ main_lista.c
@@ -7,46 +7,8 @@
  * Autor: Presley Demuner Reverdito
  * Universidade Federal de Mato Grosso
  */
-int main(){
-    printf("################################################\n");
-    printf("# Trabalho Manipulacao de Polinomios\n");
-    printf("# Disciplina Estrutura de Dados\n");
-    printf("# Prof. Frederico S. Oliveira\n");
-    printf("# Autor: Presley Demuner Reverdito RGA: 201511902022\n");
-    printf("################################################\n");
-    char pergunta,funcao;
-    int opcao,tamanho1,tamanho2,i,expoente,escalar;
-    Poli *f,*g,*derivado;
-    float x;
-    while(pergunta == 'y' || pergunta == 'Y'){
-        printf("Digite o tamanho do Polinomio F");
-        scanf("%d",&tamanho1);
-        int vetor1[tamanho1];
-        if(tamanho1>MAX){
-            printf("valor excedido");
-            return 0;
-        }
-        printf("Digite os valores do Polinomio F");
-        for(i=0;i<tamanho1;i++){
-            scanf("%d",vetor1[i]);
-        }
 
-
-        printf("Digite o tamanho do Polinomio G");
-        scanf("%d",&tamanho2);
-         if(tamanho2>MAX){
-            printf("valor excedido");
-            return 0;
-        }
-        int vetor2[tamanho2];
-        printf("Digite os valores do Polinomio G");
-        for(i=0;i<tamanho2;i++){
-            scanf("%d",vetor2[i]);
-        }
-
-        f =criaPolinomio(vetor1,tamanho1);
-        g =criaPolinomio(vetor2,tamanho2);
-
+ void menu(){
         printf("O que o Senhor(a) deseja fazer?\n");
         printf("1- Adiconar termo?\n");
         printf("2- Apagar termo?\n");
@@ -57,35 +19,83 @@ int main(){
         printf("7- Multiplicar polinomio?\n");
         printf("8- Derivar um polinomio?\n");
         printf("9- Calcular o polinomio com o valor real x\n");
+ }
+ void cabecalho(){
+    printf("################################################\n");
+    printf("# Trabalho Manipulacao de Polinomios\n");
+    printf("# Disciplina Estrutura de Dados\n");
+    printf("# Prof. Frederico S. Oliveira\n");
+    printf("# Autor: Presley Demuner Reverdito RGA: 201511902022\n");
+    printf("################################################\n");
+
+ }
+int main(){
+    cabecalho();
+    char pergunta ,funcao= NULL;
+    int opcao,tamanho1,tamanho2,i,expoente,escalar;
+    Poli *f,*g,*derivado;
+    float x;
+    pergunta='y';
+    while(pergunta == 'y'){
+        printf("Digite o tamanho do Polinomio F");
+        scanf("%d",&tamanho1);
+        int vetor1[tamanho1];
+        if(tamanho1>MAX){
+            printf("Valor excedido");
+            return;
+        }
+        printf("Digite os valores do Polinomio F");
+        for(i=0;i<tamanho1;i++){
+            scanf("%d",&vetor1[i]);
+        }
+
+        printf("Digite o tamanho do Polinomio G");
+        scanf("%d",&tamanho2);
+        int vetor2[tamanho2];
+        if(tamanho2>MAX){
+            printf("Valor excedido");
+            return;
+        }
+        printf("Digite os valores do Polinomio G");
+        for(i=0;i<tamanho2;i++){
+            scanf("%d",&vetor2[i]);
+        }
+
+        f =criaPolinomio(vetor1,tamanho1);
+        g =criaPolinomio(vetor2,tamanho2);
+
+        menu();
         scanf("%d",&opcao);
 
-        switch (opcao){
+
+       switch (opcao){
            case 1:
              printf("Insira polinomio, o expoente e o coeficiente desejado\n");
              printf("f Para o primeiro e g para o segundo\n");
-             scanf("%c %d %d",funcao, &expoente, &escalar);
-             if (funcao =='f')
-                adicionaTermo(*f, expoente,escalar);
+             scanf("%c %d %d",&funcao, &expoente, &escalar);
+             if (funcao =='f'){
+                adicionaTermo(&f, expoente,escalar);
+             }
              else
-                adicionaTermo(*g, expoente,escalar);
+                adicionaTermo(&g, expoente,escalar);
              printf("Termo adicionado\n");
            break;
 
            case 2:
              printf("Insira o polinomio e o grau a ser exluido\n");
              printf("f Para o primeiro e g para o segundo\n");
-             scanf("%c %d",funcao, &expoente);
+             scanf("%c %d",&funcao, &expoente);
              if (funcao =='f')
-                apagaTermo(*f,expoente);
+                apagaTermo(&f,expoente);
             else
-                apagaTermo(*g,expoente);
+                apagaTermo(&g,expoente);
              printf("Termo apagado!");
            break;
 
            case 3:
              printf("Qual polinomio deseja saber o grau?\n");
              printf("f Para o primeiro e g para o segundo\n");
-             scanf("%c",funcao);
+             scanf("%c",&funcao);
              if (funcao =='f')
                 printf("%d",grauPolinomio(f));
              else
@@ -96,7 +106,7 @@ int main(){
            case 4:
              printf("Insira o polinomio e o grau que deseja para saber o coefiente?\n");
              printf("f Para o primeiro e g para o segundo\n");
-             scanf("%c %d",funcao,&expoente);
+             scanf("%c %d",&funcao,&expoente);
              if (funcao =='f')
                 printf("O coeficiente é:%d\n",coeficiente(f,expoente));
              else
@@ -118,7 +128,7 @@ int main(){
            case 8:
              printf("Qual polinomio deseja derivar?");
              printf("f Para o primeiro e g para o segundo\n");
-             scanf("%c",funcao);
+             scanf("%c",&funcao);
              if (funcao =='f')
                 derivado = derivaPolinomio(f);
              else
@@ -130,7 +140,7 @@ int main(){
            case 9:
              printf("Insere o polinomio e o valor de x\n");
              printf("f Para o primeiro e g para o segundo\n");
-             scanf("%c %d",funcao,&x);
+             scanf("%c %d",&funcao,&x);
              if (funcao =='f')
                 valorPolinomio(f,x);
              else
@@ -138,20 +148,14 @@ int main(){
 
            break;
 
-           default
-                printf("Deseja continuar?(y/n)");
-                scanf("%c",&pergunta);
+
+            printf("Deseja continuar?(y/n)");
+            scanf("%c",&pergunta);
+
         }
 
-
-
-
-
-
-
-
     }
-printf("Programa finalizado");
+printf("\nPrograma finalizado/n");
 
 }
 
